1a66e3f51e44567c5d7f9f2319aba673a79c12cf
Dev <dev1ender@gmail.com>
2023-08-10 16:10:28 -0400

    Fixed UI and added logs to the backend

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, Response, render_template, request,make_response
+from flask import Flask, Response, render_template, request, make_response
 import cv2
 import base64
 import numpy as np
@@ -6,6 +6,11 @@ import random
 import string
 import os
 import time
+import logging
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 # Path to the folder where processed frames will be saved
 PROCESSED_FRAMES_FOLDER = os.path.join(os.path.dirname(__file__), 'processed_frames')
@@ -13,18 +18,17 @@ PROCESSED_FRAMES_FOLDER = os.path.join(os.path.dirname(__file__), 'processed_fra
 # Ensure the folder exists
 os.makedirs(PROCESSED_FRAMES_FOLDER, exist_ok=True)
 
-
 app = Flask(__name__)
 
 # Processing function
-def process_frame(frame_data,unique_number):
-    # Decode base64 image data
+def process_frame(frame_data, unique_number):
+    logger.info(f"Processing frame with unique number: {unique_number}")
+    
     image_data = frame_data.split(',')[1]
     decoded_data = base64.b64decode(image_data)
     nparr = np.frombuffer(decoded_data, np.uint8)
     frame = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-   
-    # Add the unique number to the frame using OpenCV
+
     font = cv2.FONT_HERSHEY_SIMPLEX
     bottom_left_corner = (10, 30)
     font_scale = 1
@@ -33,11 +37,9 @@ def process_frame(frame_data,unique_number):
 
     cv2.putText(frame, unique_number, bottom_left_corner, font, font_scale, font_color, line_type)
 
-    # Encode the modified frame back to JPEG format
     _, buffer = cv2.imencode('.jpg', frame)
-    # modified_frame_data = base64.b64encode(buffer).decode('utf-8')
     modified_frame_data = buffer.tobytes()
-    
+
     timestamp = int(time.time())
 
     response = {
@@ -46,37 +48,34 @@ def process_frame(frame_data,unique_number):
         "timestamp": timestamp
     }
 
-    # Create a JSON response
     json_response = make_response(response)
     json_response.headers['Content-Type'] = 'application/json'
 
     return json_response
 
-def display_base64_image(base64_data, unique_number):
-    image_data = base64_data.split(',')[1]
-    decoded_data = base64.b64decode(image_data)
-    image_path = os.path.join(PROCESSED_FRAMES_FOLDER, f'frame-{unique_number}.jpg')
-    with open(image_path, 'wb') as f:
-        f.write(decoded_data)
-    print(f"Image saved to {image_path}")
-
-
 @app.route('/')
 def index():
+    logger.info("Rendering index page")
     return render_template('index.html')
 
 @app.route('/process_frame', methods=['POST'])
 def process_frame_route():
-    data = request.json
-    frame_data = data.get('frame', '')
-    # Generate a unique 10-digit number
-    unique_number = ''.join(random.choice(string.digits) for _ in range(10))
-    # display_base64_image(frame_data, unique_number)
-    if frame_data:
-        result = process_frame(frame_data,unique_number)
-        return result
+    try:
+        data = request.json
+        frame_data = data.get('frame', '')
+        unique_number = ''.join(random.choice(string.digits) for _ in range(10))
+        
+        if frame_data:
+            result = process_frame(frame_data, unique_number)
+            logger.info(f"Frame processed successfully with unique frame number: {unique_number}")
+            return result
 
-    return 'Frame data not received', 400
+        logger.warning("Frame data not received")
+        return 'Frame data not received', 400
+
+    except Exception as e:
+        logger.error(f"An error occurred: {str(e)}")
+        return 'An error occurred', 500
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
